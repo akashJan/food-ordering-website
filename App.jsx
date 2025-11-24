6aa99cd25e463999403e3f4678a7436e75b29a95
Akash Jan <116725881+akashJan@users.noreply.github.com>
2025-11-24 17:45:16 +0530

    project-created

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import React, { createElement } from "react";
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () => {
+  return;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
